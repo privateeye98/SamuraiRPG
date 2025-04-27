02bd01452db7c468c4fbeea2718f26ed14f042e3
KBEOMSU <makeurselfff@gmail.com>
2025-04-27 21:15:06 +0900

    프로젝트 초기

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float maxSpeed;
+
+     void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {   //Jump
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity.nomalized; // Jump force
+            rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        }
+    }
+    void FixedUpdate()
+    {
+        // Player Movement
+        float h = Input.GetAxisRaw("Horizontal");
+        rb.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+
+        // Clamp the player's speed
+        rb.linearVelocity = new Vector2(Mathf.Clamp(rb.linearVelocity.x, -maxSpeed, maxSpeed), rb.linearVelocity.y);
+    }
+}
